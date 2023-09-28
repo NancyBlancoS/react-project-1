99165f90576de79cc96d0284e5a711aa7ebe15d4
Nancy <nancy.blanco.suarez@gmail.com>
2023-09-27 21:05:42 -0500

    React-project-2

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,23 @@ import './App.css';
 import Avatar from './components/avatar/avatar';
 import Badge from './components/bagde/badge';
 import Card from './components/card/card';
+import Counter from './components/counter/counter';
 import Footer from './components/footer/footer';
 import Header from './components/header/header';
-
-import dandelion from './components/avatar/dandelion.jpg'
+import dandelion from './components/avatar/dandelion.jpg';
 
 function App() {
   return (
     <div className = "App">
         <Header title = "React Beginner's Quiz"/>
 
-        <Card title = "Card Title 1" content = "Card Conntent 1"/>
-        <Card title = "Card Title 2" content = "Card Conntent 2"/>
-        <Card title = "Card Title 3" content = "Card Conntent 3"/>
+        <Card title = "Card Title 1" content = "Card Content 1"/>
+        <Card title = "Card Title 2" content = "Card Content 2"/>
+        <Card title = "Card Title 3" content = "Card Content 3"/>
+        <Counter />
 
         <Avatar imageSrc = {dandelion} content = "dandelion"/>
-        
+
         <Footer content="Created with ❤️ by Nancy Blanco"/>
         <Badge label = "New!"/>
     </div>
--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -6,6 +6,7 @@ function Card({title,content}){
         <div className="card">
             <h2>{title}</h2>
             <p>{content}</p>
+
         </div>
     )
 }
